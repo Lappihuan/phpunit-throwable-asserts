37b9578389f58724f802dc6009ff5f8a3413703c
Oriano de-Stefani <ode@exigo.org>
2025-03-12 20:51:10 +0100

    try exception fix

--- src/InvalidArrayAssertTestArgumentException.php
+++ src/InvalidArrayAssertTestArgumentException.php
@@ -9,9 +9,19 @@ use Throwable;
 
 class InvalidArrayAssertTestArgumentException extends InvalidArgumentException
 {
+#Expected :'Argument #2 of PhrozenByte\PHPUnitArrayAsserts\Assert::assertAssociativeArray() must be an array or ArrayAccess'
+#Actual   :'Argument 2 of PhrozenByte\PHPUnitThrowableAsserts\ArrayAssert is invalid: array or ArrayAccess'
+
     public static function create(int $arg, string $message): self {
+        $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+        $caller = $backtrace[1];
+        
+        $class = $caller['class'] ?? '';
+        $function = $caller['function'] ?? '';
+        $method = $class ? "$class::$function" : $function;
+
         return new self(
-            sprintf('Argument %d of %s is invalid: %s', $arg, ArrayAssert::class, $message)
+            sprintf('Argument #%d of %s is invalid: %s', $arg, $method, $message)
         );
     }
 }
