8d3ca44650e071de10f226a35ef2abc149009a0e
Oriano de-Stefani <ode@exigo.org>
2025-03-12 20:45:59 +0100

    fix exception message

--- src/InvalidArrayAssertTestArgumentException.php
+++ src/InvalidArrayAssertTestArgumentException.php
@@ -9,7 +9,9 @@ use Throwable;
 
 class InvalidArrayAssertTestArgumentException extends InvalidArgumentException
 {
-    public static function create(int $code, string $message): self {
-        return new self($message, $code);
+    public static function create(int $arg, string $message): self {
+        return new self(
+            sprintf('Argument %d of %s is invalid: %s', $arg, ArrayAssert::class, $message)
+        );
     }
 }
